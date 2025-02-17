1dee39a18a1446bd57424a47dedc793a60488ecb
Jagadeesh Nalamalapu <160627307+Jagadeeshreddy14@users.noreply.github.com>
2025-02-17 23:02:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -48,5 +48,5 @@ server.get("/",(req,res)=>{
 })
 
 server.listen(8000,()=>{
-    console.log('server [STARTED] ~ http://localhost:8000');
+    console.log('server [STARTED] ~https://eccomerce-website-backend.onrender.com');
 })
